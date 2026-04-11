c18d936c44ca71315a26817c436a20e6f4d63667
pohlmary <iftixortoychoyev1@gmail.com>
2026-04-11 22:07:19 +0500

    fix: remove duplicate BOT_TOKEN and ADMIN_TELEGRAM_ID definitions in config.php

--- bot/config.php
+++ bot/config.php
@@ -6,7 +6,6 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'olmazorgo');
 define('DB_USER', 'olmazorgo');
 define('DB_PASS', 'Iftixor2006');
-<?php
 define('BOT_TOKEN', '8695815144:AAGN5duKthrGjEake5hk88QArLsQZrgHAzg');
 define('ADMIN_TELEGRAM_ID', 7820936794);
 define('WEBAPP_URL', 'https://olmazorgo.bigsaver.ru/webapp/index.php?v=67');
